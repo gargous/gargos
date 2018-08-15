cabd94c74c7ba04f1627085b2864ebc2f2dbd314
xiongzhen <xiongzhen@nibirutech.com>
2018-08-15 17:08:22 +0800

    fix

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ GOARCH=386
 apt install nasm -y &&
 nasm -f elf32 kernel.asm -o kasm.o &&
 rm entry.o
-go tool compile -linkobj entry.l -o entry.o entry.go &&
-ld -m elf_i386 -T link.ld -o kernel kasm.o entry.o
+go tool compile -linkobj entry.l -o entry.o entry.go -S >> entry.s  &&
+ld -m elf_i386 -T link.ld -o kernel kasm.o entry.s
